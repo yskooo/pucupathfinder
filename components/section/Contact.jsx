d5ad5994fbae531c7dd60723d254ff1bcad5e38f
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-03-01 22:41:39 +0800

    basta nacommit yung update pagod nako

--- components/section/Contact.jsx
+++ components/section/Contact.jsx
@@ -1,29 +1,4 @@
-// import React from 'react';
-
-// const Contact = () => {
-//   return (
-//     <section className="bg-gray-100 mx-auto my-auto flex justify-center items-center h-screen" id='contact'>
-//     <div className=" px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-//         <div className="mx-auto max-w-screen-sm text-center">
-//             <h2 className="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">Get in Touch</h2>
-//             <p className="mb-6 font-light text-gray-500 md:text-lg">Do you have a feature request or you want to feature your research study in this platform?</p>
-//             <button>
-//               <a href="mailto:pucupathfinder@gmail.com" className="text-blue-900 bg-transparent border-2 border-blue-900 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
-//                 Send an Email
-//               </a>
-//             </button>
-//         </div>
-//     </div>
-// </section>
-//   )
-// }
-
-// export default Contact;
-
-
-
 import React from 'react'
-
 
 const Contact = () => {
   return (
